30cfc1301754644425b06133a93ba176d38908dd
Tiqur <trevorbrage02@gmail.com>
2023-09-30 14:17:09 -0700

    init tampermonkey script

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,1 +1,13 @@
-console.log('test');
+// ==UserScript==
+// @name         HeadlessYT
+// @version      0.1
+// @description  A simple script to expose an API for the youtube media player.
+// @author       TrevorBrage
+// @match        https://www.youtube.com/watch?v=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
